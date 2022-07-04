3bd8d7e2a9fc4e1484d48b5fbb5f6128727af69a
Siddharth Ashweni <72736772+SiddharthGameDev@users.noreply.github.com>
2022-07-04 13:03:42 +0530

    Initial commit

---
+++ Assets/CharacterSelection-main/Assets/Scripts/TankSelection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    public TankSpwaner tankSpwaner;
+    public void BlueTankSelected()
+    {
+        Debug.Log("Blue Tank Selected");
+        tankSpwaner.CreateTank(TankTypes.BlueTank);
+    }
+    public void GreenTankSelected()
+    {
+        tankSpwaner.CreateTank(TankTypes.GreenTank);
+    }
+  
+    public void RedTankSelected()
+    {
+        tankSpwaner.CreateTank(TankTypes.RedTank);
+    }
+
+}
